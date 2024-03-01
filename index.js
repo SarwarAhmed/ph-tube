33d404735e559670385c5a08dbf1cc687535a3ff
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-03-01 12:43:29 +0600

    Update dynamic card data

--- index.js
+++ index.js
@@ -29,25 +29,30 @@ const fetchDataByCategory = (categoryID) => {
             cardContainer.innerHTML = '';
 
             data.forEach((video) => {
+                let verifiedBadge = '';
+                if (video.authors[0].verified) {
+                    verifiedBadge = `<img class="w-6 h-6" src="./images/verify.png" alt="">`;
+                }
                 const newCard = document.createElement('div');
                 newCard.innerHTML = `
                 <div class="card w-full bg-base-100 shadow-xl">
                     <figure class="overflow-hidden h-72">
-                        <img class="w-full" src="./images/smells.jpg" alt="Shoes" />
+                        <img class="w-full" src="${video.thumbnail}" alt="Shoes" />
                         <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
                     </figure>
                     <div class="card-body">
                         <div class="flex space-x-4 justify-start items-start">
                             <div>
-                                <img class="w-12 h-12 rounded-full" src="./images/smells.jpg" alt="Shoes" />
+                                <img class="w-12 h-12 rounded-full" src="${video.authors[0].profile_picture}" alt="Shoes" />
                             </div>
                             <div>
-                                <h2 class="card-title">Laugh At My Pain</h2>
+                                <h2 class="card-title">${video.title}</h2>
                                 <div class="flex mt-3">
-                                    <p class="">Author Name</p>
-                                    <img class="w-6 h-6" src="./images/verify.png" alt="">
+                                    <p class="">${video.authors[0].profile_name}</p>
+                                    ${/* <img class="w-6 h-6" src="images/verify.png" alt=""> */''}
+                                    ${verifiedBadge}
                                 </div>
-                                <p class="mt-3">100k Views</p>
+                                <p class="mt-3">${video.others.views} Views</p>
                             </div>
                         </div>
                     </div>
